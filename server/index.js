8808c77ec9ddb61a597a98e69af9b3a540caaad3
joyumoekpo <joyumoekpo1@gmail.com>
2022-12-26 20:29:36 -0600

    Back End 1 Exercise

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ app.get("/weather/:temperature", (req, res) => {
     } = req.params;
     const phrase = `<h3>It was ${temperature} today</h3>`;
     res.status(200).send(phrase);
-});
+}); 
 
 app.listen(4000, () => console.log("Server running on port 4000"));
